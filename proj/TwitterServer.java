c7834ddfeaf16c226cb1088860f5d81063f2894f
Vaspol Ruamviboonsuk <vaspol@cs.washington.edu>
2013-04-19 16:31:42 -0700

    Resolved some conflicts

--- proj/TwitterServer.java
+++ proj/TwitterServer.java
@@ -5,6 +5,11 @@
 
 public class TwitterServer extends RIONode {
 
+    public static final String CREATE = "create";
+    public static final String READ = "read";
+    public static final String UPDATE = "update";
+    public static final String DELETE = "delete";
+    
     @Override
     public void onRIOReceive(Integer from, int protocol, byte[] msg) {
         // TODO Auto-generated method stub
@@ -12,14 +17,24 @@ public class TwitterServer extends RIONode {
 
     @Override
     public void start() {
-        // TODO Auto-generated method stub
+        
     }
 
     /**
-     * 
+     * accept commands
      */
     @Override
     public void onCommand(String command) {
-        // TODO Auto-generated method stub
+        if (command.equals(CREATE)) {
+            
+        } else if (command.equals(READ)) {
+            
+        } else if (command.equals(UPDATE)) {
+            
+        } else if (command.equals(DELETE)) {
+            
+        } else {
+            throw new RuntimeException("Command not supported by the server");
+        }
     }
 }
