e2bcb3477f28139c465661306235ba5fe12ab9b6
Vaspol Ruamviboonsuk <vaspol@cs.washington.edu>
2013-04-19 15:27:23 -0700

    Add TwitterServer.java

---
+++ proj/TwitterServer.java
@@ -0,0 +1,25 @@
+/**
+ * Twitter Server that will handle requests from Twitter Client
+ * @author vaspol
+ */
+
+public class TwitterServer extends RIONode {
+
+    @Override
+    public void onRIOReceive(Integer from, int protocol, byte[] msg) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public void onCommand(String command) {
+        // TODO Auto-generated method stub
+    }
+}
