8f510aad02ee91e74d3249fadc43fa56ec652ebd
Lee Lee Choo <chooleeelee@gmail.com>
2013-04-21 15:35:00 -0700

    newest TwitterProtocol

--- proj/TwitterProtocol.java
+++ proj/TwitterProtocol.java
@@ -1,6 +1,3 @@
-import com.google.gson.JsonObject;
-
-
 /**
  * Holds the structure for a request for GSON to parse and put it into this object structure
  * 
@@ -12,9 +9,7 @@ public class TwitterProtocol {
 
   private final String method;
   private final String collection;
-  private final JsonObject data;
-  private final String key;
-  private final String operation;
+  private final String data;
 
   /**
    * Constructs a new structure to hold for communication
@@ -23,12 +18,10 @@ public class TwitterProtocol {
    * @param collection the target collection
    * @param data extra information
    */
-  public TwitterProtocol(String method, String collection, String key, String operation, JsonObject data) {
+  public TwitterProtocol(String method, String collection, String data) {
     this.method = method;
     this.collection = collection;
     this.data = data;
-    this.key = key;
-    this.operation = operation;
   }
 
   /**
@@ -48,15 +41,7 @@ public class TwitterProtocol {
   /**
    * @return the data hold in this request
    */
-  public JsonObject getData() {
+  public String getData() {
     return data;
-  }
-  
-  public String getKey() {
-	  return key;
-  }
-  
-  public String getOperation() {
-	  return operation;
   }
 }
