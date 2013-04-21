dd8ca3602422a81b69dd23a1fc44bd908d4881fa
Vaspol Ruamviboonsuk <vaspol@cs.washington.edu>
2013-04-21 11:53:02 -0700

    Modified the TwitterProtocol

--- proj/TwitterProtocol.java
+++ proj/TwitterProtocol.java
@@ -1,71 +1,50 @@
-import com.google.gson.JsonObject;
+import java.util.Map;
 
 /**
+ * Holds the structure for a request for GSON to parse and put it into this object structure
  * 
- * @author leelee
- *
+ * @author vaspol
+ * 
  */
+
 public class TwitterProtocol {
-	
-	private static final String METHOD_KEY = "method";
-	private static final String COLLECTION_KEY = "collection";
-	private static final String HASH_KEY = "hash";
-	private static final String ID_KEY = "id";
-	private static final String DATA_KEY = "data";
-	
-	private static int sequenceNumberGenerator = 0;
-	private JsonObject obj;
-	
-	public TwitterProtocol(String method, String collection, JsonObject data) {
-		obj = new JsonObject();
-		obj.addProperty(METHOD_KEY, method);
-		obj.addProperty(COLLECTION_KEY, collection);
-		obj.addProperty(ID_KEY, sequenceNumberGenerator+=1);
-		obj.add(DATA_KEY, data);
-	}
-	
-	public TwitterProtocol(String method, String collection, String hash, JsonObject data) {
-		obj = new JsonObject();
-		obj.addProperty(METHOD_KEY, method);
-		obj.addProperty(COLLECTION_KEY, collection);
-		obj.addProperty(HASH_KEY, hash);
-		obj.addProperty(ID_KEY, sequenceNumberGenerator+=1);
-		obj.add("data", data);
-	}
-	
-	public TwitterProtocol(String method, String collection, String hash) {
-		obj = new JsonObject();
-		obj.addProperty(METHOD_KEY, method);
-		obj.addProperty(COLLECTION_KEY, collection);
-		obj.addProperty(HASH_KEY, hash);
-		obj.addProperty(ID_KEY, sequenceNumberGenerator+=1);
-	}
-	
-	public TwitterProtocol(byte[] data) {
-		
-	}
-	
-	public String getMethod() {
-		return obj.get(METHOD_KEY).getAsString();
-	}
-	
-	public String getCollection() {
-		return obj.get(COLLECTION_KEY).getAsString();
-	}
-	
-	public String getHash() {
-		return obj.get(HASH_KEY).getAsString();
-	}
-	
-	public String getId() {
-		return obj.get(ID_KEY).getAsString();
-	}
-	
-	public JsonObject getData() {
-		return obj.get(DATA_KEY).getAsJsonObject();
-	}
-	
-	public byte[] getBytes() {
-		return obj.toString().getBytes();
-	}
+
+  private final String method;
+  private final String collection;
+  private final Entry data;
+
+  /**
+   * Constructs a new structure to hold for communication
+   * 
+   * @param method the method being called
+   * @param collection the target collection
+   * @param data extra information
+   */
+  public TwitterProtocol(String method, String collection, Entry data) {
+    this.method = method;
+    this.collection = collection;
+    this.data = data;
+  }
+
+  /**
+   * @return the method
+   */
+  public String getMethod() {
+    return method;
+  }
+
+  /**
+   * @return the collection
+   */
+  public String getCollection() {
+    return collection;
+  }
+
+  /**
+   * @return the data hold in this request
+   */
+  public Map<String, Object> getData() {
+    return data;
+  }
+
 }
