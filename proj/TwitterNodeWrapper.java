105ff171ffef9b52a64a6a1a84202425ceaaea42
Lee Lee Choo <chooleeelee@gmail.com>
2013-04-23 01:40:03 -0700

    fixed the onRIOReceive bugs in TwitterNodeWrapper.java

--- proj/TwitterNodeWrapper.java
+++ proj/TwitterNodeWrapper.java
@@ -1,5 +1,8 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import com.google.gson.Gson;
 
 import edu.washington.cs.cse490h.lib.PersistentStorageReader;
 import edu.washington.cs.cse490h.lib.PersistentStorageWriter;
@@ -11,19 +14,22 @@ public class TwitterNodeWrapper extends RIONode {
    */
   private Client client;
   private TwitterServer server;
+  public static final Gson GSON = new Gson();
 
   @Override
   public void onRIOReceive(Integer from, int protocol, byte[] msg) {
-    if (from == 0) {
+    if (from == 4) {
       // this is from the client
       server.onRIOReceive(from, protocol, msg);
+    } else if (from == 3) {
+    	client.onRIOReceive(from, protocol, msg);
     }
   }
 
   @Override
   public void start() {
 
-    if (super.addr == 0) {
+    if (super.addr == 3) {
       Utils.logOutput(super.addr, "Starting up server...");
 
       // Generate a user-level synoptic event to indicate that the node started.
@@ -31,14 +37,21 @@ public class TwitterNodeWrapper extends RIONode {
 
       // assuming that it is a server
       server = new TwitterServer(this);
-    } else if (super.addr == 1) {
+      server.start();
+    } else if (super.addr == 4) {
       client = new Client(this);
+      client.start();
     }
   }
 
   @Override
   public void onCommand(String command) {
-	  client.onCommand(command);
+	  try {
+		client.onCommand(command);
+	} catch (Exception e) {
+		e.printStackTrace();
+		fail();
+	}
   }
 
   /**
