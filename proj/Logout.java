b671ea46003c1a10bb9640d9c226e8d144b6525d
Lee Lee Choo <chooleeelee@gmail.com>
2013-04-23 01:37:26 -0700

    done with Logout eventloop

---
+++ proj/Logout.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.washington.cs.cse490h.lib.Callback;
+
+/**
+ * 
+ * @author leelee
+ *
+ */
+public class Logout extends Function {
+
+	private int serverAddress;
+	private String username;
+	
+	public Logout(Client client, RIONode rioNode, int serverAddress, String username) {
+		super(client, rioNode);
+		this.serverAddress = serverAddress;
+		this.username = username;
+	}
+
+	@Override
+	public List<Callback> init() {
+		List<Callback> list = new ArrayList<Callback>();
+		try {
+			list.add(new Callback(Callback.getMethod("step1", this, null), this, null));
+			list.add(new Callback(Callback.getMethod("step2", this, new String[] {"java.lang.String"}), this, null));
+		} catch (Exception e) {
+			e.printStackTrace();
+			rioNode.fail();
+		}
+		eventList = list;
+		return list;
+	}
+	
+	public void step1() {
+		// delete this user's username from login.txt
+		TwitterProtocol tpQuery = new TwitterProtocol(TwitterServer.DELETE_LINES, "login.txt", username);
+		rioNode.RIOSend(serverAddress, Protocol.DATA, tpQuery.toBytes());
+		client.eventIndex = 1;
+	}
+	
+	public void step2(String responseString) {
+		if (!responseString.startsWith(TwitterServer.SUCCESS)) {
+			logError("Error issuing request to server.");
+			return;
+		}
+		logOutput("You are logout!");
+		client.eventIndex = 0;
+		client.completeCommand();
+	}
+}
