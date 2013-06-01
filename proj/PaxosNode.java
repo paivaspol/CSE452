0f2938e2742d3555af68fdd068ce49fd058f4771
Lee Lee Choo <chooleeelee@gmail.com>
2013-06-01 13:48:42 -0700

    PaxosNode: added static variables

--- proj/PaxosNode.java
+++ proj/PaxosNode.java
@@ -8,6 +8,15 @@ public class PaxosNode {
 
 	/** the wrapper of this node */
 	private final TwitterNodeWrapper wrapper;
+	public static final String PREPARE = "prepare";
+	public static final String PROMISE = "promise";
+	public static final String ACCEPT = "accept";
+	public static final String ACCEPTED = "accepted";
+	public static final String YES = "yes";
+	public static final String NO = "no";
+	public static final String CHANGE = "change";
+	
+	
 	
 	private Set<Integer> otherPaxosNodes;
 	
@@ -35,7 +44,10 @@ public class PaxosNode {
 	
 	public void onRIOReceive(Integer from, int protocol, byte[] msg) {
 		// TODO Auto-generated method stub
-
+		// check if it is from other paxos or from file server?
+//		if () {
+//			
+//		}
 	}
 
 	public void start() {
